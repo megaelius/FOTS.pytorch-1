3ce615b13044a6419b338f1121b83fa6d5aa11f3
megaelius <abad.elias00@gmail.com>
2021-08-03 17:15:01 +0200

    tdryhtdgrfw

--- evaluation.py
+++ evaluation.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
                     draw.text((center[0], center[1]), det_text, fill = (0,255,0),font=font2)
                     out_boxes.append(box)
                     #print(det_text, conf, dec_s)
-                    if (is_plate(det_text) and conf > confidence:
+                    if is_plate(det_text) and conf > confidence:
                         plate = det_text
                         confidence = conf
                 print(plate, confidence)
