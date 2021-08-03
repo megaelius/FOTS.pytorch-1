ba3efdcfa8068e4737413fb412675fa611ce5853
megaelius <abad.elias00@gmail.com>
2021-08-03 16:54:38 +0200

    small changes in plate assingment

--- evaluation.py
+++ evaluation.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
                     draw.text((center[0], center[1]), det_text, fill = (0,255,0),font=font2)
                     out_boxes.append(box)
                     #print(det_text, conf, dec_s)
-                    if (is_plate(det_text) and conf > confidence) or (is_plate(det_text) and '-' in det_text):
+                    if (is_plate(det_text) and plate is None) or (is_plate(det_text) and '-' in det_text and plate is not None):
                         plate = det_text
                         confidence = conf
                 print(plate, confidence)
