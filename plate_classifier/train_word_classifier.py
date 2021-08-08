9bee8a0311a024b916703fd6db104afe93caafcd
megaelius <abad.elias00@gmail.com>
2021-08-08 17:35:11 +0200

    embedding dim = 2

--- plate_classifier/train_word_classifier.py
+++ plate_classifier/train_word_classifier.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    model = PlateNet(num_embeddings = len(dataset.vocabulary) + 1, embedding_dim = 8)
+    model = PlateNet(num_embeddings = len(dataset.vocabulary) + 1, embedding_dim = 2)
 
     model = model.to(device)
 
